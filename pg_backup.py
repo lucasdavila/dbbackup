d25c4819c3ca7bfc4fcba73bd5f9ced63a927867
Lucas D'Avila <lucassdvl@gmai.com>
2011-09-01 01:13:47 -0300

    corrigido erro sintaxe

--- pg_backup.py
+++ pg_backup.py
@@ -5,5 +5,5 @@ if __name__ == '__main__':
     b = Backup()
     for a in sys.argv[1:]:
         b.backup(a)
-    if len(sys.argv < 2):
+    if len(sys.argv) < 2:
         print 'Hey!!! I want to work ok? pass me the name of one or more schedules :P'
