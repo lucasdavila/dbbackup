8895839b28ff0f1ca8e4ecbb260e84651617128d
Lucas D'Avila <lucassdvl@gmai.com>
2011-09-02 02:08:44 -0300

    Adicionado licenca, alterado readme incluindo exemplos da configuração dos managers

--- pg_backup.py
+++ pg_backup.py
@@ -1,3 +1,22 @@
+# -*- coding: utf-8 -*- 
+"""
+Copyright (c) 2011 Lucas D'Avila - email <lucassdvl@gmail.com> / twitter @lucadavila
+
+This file is part of pgbackup.
+
+pgbackup is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License (LGPL v3) as published by
+the Free Software Foundation, on version 3 of the License.
+
+pgbackup is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with pgbackup.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from backup import Backup
 
 if __name__ == '__main__':
